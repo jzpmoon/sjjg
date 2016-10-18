413ad273d9a7558cc13dce4d58a00a7a013b2924
jzpmoon <2481957497@qq.com>
2016-10-18 22:01:10 +0800

    add vs2015 build

--- src/list.h
+++ src/list.h
@@ -1,9 +1,12 @@
-#pragma once
+#ifndef LIST_H
+#define LIST_H
 
 typedef struct _list list;
-struct _list { 
+struct _list {
 	void* data;
 	struct _list* next;
-	};
-list* list_insert(list* ls,void* data,int idx);
-void* list_query(list* ls,int idx);
+};
+list* list_insert(list* ls, void* data, int idx);
+void* list_query(list* ls, int idx);
+
+#endif // !LIST_H
