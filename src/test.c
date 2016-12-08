814ed457b9a1e2eeb7605082e87c8fdb2e977c96
Your Name <you@example.com>
2016-12-08 18:18:20 +0800

    2016128

--- src/test.c
+++ src/test.c
@@ -1,7 +1,8 @@
 #include "clist.h"
+#include "tree.h"
 #include <stdio.h>
 #include <windows.h>
-int seq[] = { 1,2,3,4,5 };
+int seq[] = { 4,2,3,1,5 };
 
 int cmp(void* max, void* min)
 {
@@ -40,5 +41,23 @@ void list_test()
 void tree_test()
 {
 	tree* tree=tree_new(cmp);
-	tree_insert(seq[]);
+	int i=0;
+	while(i<5)
+	{
+		tree_insert(tree,&seq[i]);
+		i++;
+	}
+	printf("pre order\n");
+	tree_pre_order(tree);
+	printf("in order\n");
+	tree_in_order(tree);
+	printf("post order\n");
+	tree_post_order(tree);
+	Sleep(5000);
+}
+
+int main()
+{
+	tree_test();
+	return 0;
 }
--- src/tree.c
+++ src/tree.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "tree.h"
 
-tree* tree_new(compare_func* cmp)
+tree* tree_new(compare_func cmp)
 {
-	tree* tree=malloc(siezof(tree));
+	tree* tree=malloc(sizeof(tree));
 	tree->root=NULL;
 	tree->cmp=cmp;
 	return tree;
@@ -11,7 +12,7 @@ tree* tree_new(compare_func* cmp)
 struct _tree_nd* tree_nd_new(void* data)
 {
 	struct _tree_nd* nd=malloc(sizeof(struct _tree_nd));
-	nd->data=NULL;
+	nd->data=data;
 	nd->left=NULL;
 	nd->right=NULL;
 	return nd;
@@ -20,9 +21,11 @@ void tree_insert(tree* tree,void* data)
 {
 	struct _tree_nd* pre_nd=NULL;
 	struct _tree_nd* nd=NULL;
-	int i=0;c=0;
+	struct _tree_nd* new_nd=NULL;
+	compare_func cmp=NULL;
+	int i=0,c=0;
 	if(!tree) return;
-	compare_func* cmp=tree->cmp;
+	cmp=tree->cmp;
 	nd=tree->root;
 	while(nd)
 	{
@@ -30,23 +33,98 @@ void tree_insert(tree* tree,void* data)
 		pre_nd=nd;
 		if(c==1)
 		{
-			nd=nd->left;
+			nd=nd->right;
 		}else if(c==-1)
 		{
-			nd=nd->right;
+			nd=nd->left;
 		}else
 		{
 			return;
 		}
 	}
-	struct _tree_nd* new_nd=tree_nd_new(data);
+	new_nd=tree_nd_new(data);
+	if(!pre_nd)
+	{
+		tree->root=new_nd;
+		return;
+	}
 	c=cmp(data,pre_nd->data);
 	if(c==1)
 	{
-		pre_nd->left=new_nd;
+		pre_nd->right=new_nd;
 	}else
 	{
-		pre_nd->right=new_nd;
+		pre_nd->left=new_nd;
+	}
+	return;
+}
+
+void tree_pre_order(tree* tree)
+{
+	struct _tree_nd* nd=NULL;
+	int top=-1;
+	void* stack[1000];
+	if(!tree) return;
+	nd=tree->root;
+	while(1){
+		if(nd){
+			printf("node data:%d\n",*(int*)nd->data);
+			if(nd->right)
+				stack[++top]=nd->right;
+			nd=nd->left;
+		}else{
+			if(top==-1) break;
+			nd=stack[top--];
+		}
+	}
+	return;
+}
+void tree_in_order(tree* tree)
+{
+	struct _tree_nd* nd=NULL;
+	int top=-1;
+	void* stack[1000];
+	if(!tree) return;
+	nd=tree->root;
+	while(1){
+		if(nd){
+			stack[++top]=nd;
+			nd=nd->left;
+		}else{
+			if(top==-1) break;
+			nd=stack[top--];
+			printf("node data:%d\n",*(int*)nd->data);
+			nd=nd->right;
+		}
+	}
+	return;
+}
+
+void tree_post_order(tree* tree)
+{
+	struct _tree_nd* pre_nd=NULL;
+	struct _tree_nd* nd=NULL;
+	int top=-1;
+	void* stack[1000];
+	if(!tree) return;
+	nd=tree->root;
+	while(1){
+		if(nd){
+			stack[++top]=nd;
+			nd=nd->left;
+		}else{
+			if(top==-1) break;
+				nd=stack[top--];
+			if(!nd->left&&!nd->right || nd->left&&!nd->right&&nd->left==pre_nd || nd->right&&nd->right==pre_nd){
+				printf("node data:%d\n",*(int*)nd->data);
+				pre_nd=nd;
+				if(top==-1) break;
+					nd=stack[top--];
+			}else{
+				stack[++top]=nd;
+				nd=nd->right;
+			}
+		}
 	}
 	return;
 }
